f7255d88afacc4b3e46743a9e602260b34d2ff03
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-31 13:01:54 -0800

    add similarity function

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models and database functions for Ratings project."""
 
 from flask_sqlalchemy import SQLAlchemy
+import correlation
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -29,6 +30,26 @@ class User(db.Model):
         """Provide helpful representation when printed."""
 
         return "<User user_id={} email={}>".format(self.user_id, self.email)
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+
+        else:
+            return 0.0
 
 
 class Movie(db.Model):
